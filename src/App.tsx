f46bae44aba26a835e0bed0e29496dbb439ff067
Angel Hurtado <sudo@angelxehg.com>
2021-05-25 09:41:14 -0500

    Instalar Bootstrap y React Bootstrap

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
 import React from 'react';
+import Container from 'react-bootstrap/Container';
 
 function App() {
   return (
-    <div >
+    <Container fluid className="pt-3 pb-3">
       <header>
         <h1>StudyLater App</h1>
         <p>Hola mundo!</p>
       </header>
-    </div>
+    </Container>
   );
 }
 
