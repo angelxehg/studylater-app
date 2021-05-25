19ec580b755193a232a1e65b500f38f337c36a7d
Angel Hurtado <sudo@angelxehg.com>
2021-05-25 10:53:55 -0500

    Mostrar versión 0.1.1

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ function App() {
   return (
     <Container fluid className="pt-3 pb-3">
       <header>
-        <h1>StudyLater App</h1>
+        <h1>StudyLater App v0.1.1</h1>
         <p>Hola mundo!</p>
       </header>
     </Container>
