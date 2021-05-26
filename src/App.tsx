7da121ba6b11f4573afe60e8a56d4fb6be54f1a6
Angel Hurtado <sudo@angelxehg.com>
2021-05-25 21:06:12 -0500

    Mostrar versión 0.2.0

--- src/App.tsx
+++ src/App.tsx
@@ -11,7 +11,7 @@ function App() {
   return (
     <Container fluid className="pt-3 pb-3">
       <header>
-        <h1>StudyLater App v0.1.2</h1>
+        <h1>StudyLater App v0.2.0</h1>
         <p>Lleva contigo tus PDFs y estudia más tarde</p>
       </header>
       <AuthCheck fallback={<LoginPage />}>
